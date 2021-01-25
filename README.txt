fdb0c09077f5a165194fc1817c07192b99d8723e
Yoshito924 <kimu.murd.cisum@gmail.com>
2021-01-25 12:15:56 +0900

    1st commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+
+pdfを画像(PEGやPNG)ファイルに一括で変換するPythonスクリプトです。
+
+詳しい説明はこちら↓。
+https://khufrudamonotes.com/pdf-to-image-python
---
+++ pdf_to_PNG_dpi150.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from pdf2image import convert_from_path
+import glob
+
+# poppler/binを環境変数PATHに追加する
+poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
+os.environ["PATH"] += os.pathsep + str(poppler_dir)
+
+# PDFファイルのパスを取得し順番に捌いていく
+
+for x in glob.glob("./pdf_file/*.pdf"):
+    pdf_path = Path(x)
+
+    # PDF -> Image に変換（150dpi）
+    pages = convert_from_path(str(pdf_path), dpi=150)
+
+    # 画像ファイルを１ページずつ保存
+    image_dir = Path("./image_file")
+    for i, page in enumerate(pages):
+        file_name = pdf_path.stem + "_{:02d}".format(i + 1) + ".png"
+        image_path = image_dir / file_name
+        # PNGで保存
+        page.save(str(image_path), "PNG")
