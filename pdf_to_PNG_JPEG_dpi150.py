dbd77c0234d3db54d11744d94f04f90478ab0788
yoshito924 <kimu.murd.cisum@gmail.com>
2021-10-18 13:59:32 +0900

    リファクタリング

--- pdf_to_PNG_JPEG_dpi150.py
+++ pdf_to_PNG_JPEG_dpi150.py
@@ -7,6 +7,15 @@ import glob
 poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
 os.environ["PATH"] += os.pathsep + str(poppler_dir)
 
+# 変換したい拡張子の情報をinput関数で受け取る
+file_extension = input('jpeg→「j」、png→「p」を入力してください。')
+
+# 拡張子の条件分岐
+if file_extension == 'j':
+    file_extension_name = 'jpeg'
+else:
+    file_extension_name = 'png'
+
 # PDFファイルのパスを取得し順番に捌いていく
 for x in glob.glob("./pdf_file/*.pdf"):
     pdf_path = Path(x)
@@ -19,7 +28,7 @@ for x in glob.glob("./pdf_file/*.pdf"):
 
     for i, page in enumerate(pages):
         # ファイルネームを決定する。
-        file_name = f'{pdf_path.stem}_{i + 1:02}.png'
+        file_name = f'{pdf_path.stem}_{i + 1:02}.{file_extension_name}'
         image_path = f'{image_dir}/{file_name}'
         # PNGで保存
-        page.save(image_path, "PNG")
+        page.save(image_path, file_extension_name)
--- pdf_to_PNG_or_JPEG_dpi350.py
+++ pdf_to_PNG_or_JPEG_dpi350.py
@@ -7,19 +7,28 @@ import glob
 poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
 os.environ["PATH"] += os.pathsep + str(poppler_dir)
 
+# 変換したい拡張子の情報をinput関数で受け取る
+file_extension = input('jpeg→「j」、png→「p」を入力してください。')
+
+# 拡張子の条件分岐
+if file_extension == 'j':
+    file_extension_name = 'jpeg'
+else:
+    file_extension_name = 'png'
+
 # PDFファイルのパスを取得し順番に捌いていく
 for x in glob.glob("./pdf_file/*.pdf"):
     pdf_path = Path(x)
 
     # pdfから画像に変換
-    pages = convert_from_path(pdf_path, dpi=150)
+    pages = convert_from_path(pdf_path, dpi=350)
 
     # 画像ファイルを1ページずつ「image_file」に保存する。
     image_dir = Path("./image_file")
 
     for i, page in enumerate(pages):
         # ファイルネームを決定する。
-        file_name = f'{pdf_path.stem}_{i + 1:02}.png'
+        file_name = f'{pdf_path.stem}_{i + 1:02}.{file_extension_name}'
         image_path = f'{image_dir}/{file_name}'
         # PNGで保存
-        page.save(image_path, "PNG")
+        page.save(image_path, file_extension_name)
